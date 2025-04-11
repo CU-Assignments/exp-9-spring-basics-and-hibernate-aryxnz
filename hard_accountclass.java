0121498d966b49b2c7f838d79253df6df85a1d6f
aryxnz <160301309+aryxnz@users.noreply.github.com>
2025-04-11 11:42:47 +0530

    Create hard_accountclass.java

---
+++ hard_accountclass.java
@@ -0,0 +1,14 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private double balance;
+
+    // Getters and Setters
+}
